4e8a38e7b36c744ce502345abe84aa95c779dc08
park, kwangjoon <kwdark1412@gmail.com>
2025-09-17 13:53:17 +0900

    three

--- src/c123456789/Main.java
+++ src/c123456789/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	
 	public static void main(String[] args) { 
 		System.out.println("김정은");
+		world.run();
 	}
 }
 	
---
+++ src/c123456789/world.java
@@ -0,0 +1,6 @@
+public class world {
+
+    public static void run(){
+        System.out.println("123456789");
+    }
+}
